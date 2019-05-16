ece06cc7a401730d8b50867cafbb78d35549c330
Hidenori Fujimura <hidenori.fujimura@unlb.dpko.un.org@dfs-geoshape-61.dpko.un.org>
2019-05-16 15:51:30 +0200

    update

---
+++ config/farms.js
@@ -0,0 +1,35 @@
+const config = require('config')
+const a320Path = config.get('a320Path')
+const cs100Path = config.get('cs100Path')
+
+module.exports = {
+  a320: { 
+    getMbtilesPath: (z, x, y) => {
+      if (z < 6) {
+        return `${a320Path}/0-0-0.mbtiles`
+      } else {
+        return `${a320Path}/6-${x >> (z - 6)}-${y >> (z - 6)}.mbtiles`
+      }
+    }
+  },
+  lc: {
+    getMbtilesPath: (z, x, y) => {
+      return `${cs100Path}/lc.mbtiles`
+    }
+  },
+  hs: {
+    getMbtilesPath: (z, x, y) => {
+      return `${cs100Path}/hs.mbtiles`
+    }
+  },
+  ocean: {
+    getMbtilesPath: (z, x, y) => {
+      return `${cs100Path}/ocean.mbtiles`
+    }
+  },
+  others: {
+    getMbtilesPath: (z, x, y) => {
+      return `${cs100Path}/others.mbtiles`
+    }
+  }
+}
--- index.js
+++ index.js
@@ -3,12 +3,29 @@ const fs = require('fs')
 const express = require('express')
 const MBTiles = require('@mapbox/mbtiles')
 const vtpbf = require('vt-pbf')
-const mapnik = require('mapnik')
 const zlib = require('zlib')
+
+const farms = require(config.get('farmsPath'))
+const port = config.get('port')
+const htdocsPath = config.get('htdocsPath')
 
 const emptyTile = vtpbf({ features: [] })
 const etag = 'a'
 
 let mbtilesPool = {}
 
+const tile2long = (x, z) => {
+  return x / 2 ** z * 360 - 180
+}
+
+const tile2lat = (y, z) => {
+  const n = Math.PI - 2 * Math.PI * y / 2 ** z
+  return 180 / Math.PI * Math.atan(0.5 * (Math.exp(n) - Math.exp(-n)))
+}
+
+const app = express()
+app.use(express.static(htdocsPath))
+
+
+app.listen(port)
 
